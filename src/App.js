642470d13c9f3b64bb91561157af71f22fa56244
steve <steve.nguyen2013@gmail.com>
2022-07-03 21:47:43 -0700

    what am I doing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch(`http://localhost:3000/Forex`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((r) => r.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">HELLOHELLO!</header>
+      <h1>TEST TEST</h1>
     </div>
   );
 }
 
 export default App;
+
+//https://exchangerate.host/#/docs
+//https://v5.reactrouter.com/web/api/Route/route-render-methods
+//https://www.forextime.com/trading-tools/currency-converter
+//https://github.com/CarlosEscat/forex-app
+//https://codepen.io/DeLeon/pen/kyGMRm
+//https://stackoverflow.com/questions/60814704/flags-inside-a-react-bootstrap-select-option-renders-as-object-object
+//https://mdbootstrap.com/docs/b5/react/content-styles/flags/
